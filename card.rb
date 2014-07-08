4ee55730b75ac77e4a6deb91d778c74c51b2054d
XaV1Uzz <xaviuzz@gmail.com>
2014-07-08 20:04:58 +0200

    Card When comparing cards knows if is equal than other card GREEN

--- card.rb
+++ card.rb
@@ -3,6 +3,7 @@ class Card
   FACE_VALUES = ["2","3","4","5","6","7","8","9","T","J","Q","K","A"]
   HIGHER = 1
   LOWER = -1
+  EQUAL = 0 
   attr_reader :description
 
   def initialize(description)
@@ -12,6 +13,7 @@ class Card
   def <=> another_card
     result = HIGHER if value(@description) > value(another_card.description)
     result = LOWER if value(@description) < value(another_card.description)
+    result = EQUAL if value(@description) == value(another_card.description)
     result  
   end
 
