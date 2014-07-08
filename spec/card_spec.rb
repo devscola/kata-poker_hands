8a16c25e8e7c88ec4c021a0769cfbc0688eff23a
XaV1Uzz <xaviuzz@gmail.com>
2014-07-08 19:57:44 +0200

    Card knows if is lower than other card RED

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,10 +2,15 @@ require './card'
 
 describe "Card" do
   
-  it "can be compared by face value" do
+  it "knows if is higher than other card" do
     a_card=Card.new("AS")
     another_card= Card.new("9D")
     expect(a_card <=> another_card).to eq(1)
   end
 
+  it "knows if is lower than other card" do
+    a_card=Card.new("AS")
+    another_card= Card.new("9D")
+    expect(another_card <=> a_card).to eq(-1)
+  end
 end
