c3211c687cd63009ea108de2c01f68f972267c7c
XaV1Uzz <xaviuzz@gmail.com>
2014-07-08 19:55:23 +0200

    Card can be compared by face value GREEN

--- card.rb
+++ card.rb
@@ -1,10 +1,19 @@
 class Card
+  
+  FACE_VALUES = ["2","3","4","5","6","7","8","9","T","J","Q","K","A"]
+  attr_reader :description
 
   def initialize(description)
-    
+    @description = description
   end
   
   def <=> another_card
+     return 1 if value(@description) > value(another_card.description)
   end
-  
+
+private
+
+  def value card
+    FACE_VALUES.index(card[0,1])
+  end 
 end
