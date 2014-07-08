2d8ce4c19cb4e62ee183af426858d8ff9b733f51
XaV1Uzz <xaviuzz@gmail.com>
2014-07-08 13:17:18 +0200

    A croupier in the casino  when checking a player hand identifies the highest card RED

--- spec/croupier_spec.rb
+++ spec/croupier_spec.rb
@@ -3,7 +3,8 @@ require './croupier'
 describe "A croupier in the casino " do
 	context 'when checking a player hand' do
     it "identifies the highest card" do
-      expect(Croupier.check_hand("2H 3D 5S 9C KD")).to eq("Highest card : KD")      
+      expect(Croupier.check_hand("2H 3D 5S 9C KD")).to eq("Highest card : KD")
+      expect(Croupier.check_hand("2H 3D AS 9C KD")).to eq("Highest card : AS")      
     end
   end
 end
