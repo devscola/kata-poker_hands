915b21c81d65319acf956f13d98c9cfafcf59ecd
XaV1Uzz <xaviuzz@gmail.com>
2014-07-08 20:00:42 +0200

    Card knows if is lower than other card REFAC

--- card.rb
+++ card.rb
@@ -1,6 +1,8 @@
 class Card
   
   FACE_VALUES = ["2","3","4","5","6","7","8","9","T","J","Q","K","A"]
+  HIGHER = 1
+  LOWER = -1
   attr_reader :description
 
   def initialize(description)
@@ -8,8 +10,9 @@ class Card
   end
   
   def <=> another_card
-     return 1 if value(@description) > value(another_card.description)
-     return -1 if value(@description) < value(another_card.description)
+    result = HIGHER if value(@description) > value(another_card.description)
+    result = LOWER if value(@description) < value(another_card.description)
+    result  
   end
 
 private
