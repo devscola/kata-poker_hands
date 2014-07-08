166445dda2545ce5010f716281e631ae0f3a6d25
XaV1Uzz <xaviuzz@gmail.com>
2014-07-08 16:40:21 +0200

    A croupier in the casino  when checking a player hand identifies the highest card REFAC

--- croupier.rb
+++ croupier.rb
@@ -1,6 +1,7 @@
 class Croupier
   RANK="Highest card"
   SEPARATOR=" : "
+  FACE_VALUES=["2","3","4","5","6","7","8","9","T","J","Q","K","A"]
 
   def self.check_hand a_hand
     highest_card = highest_card (a_hand)
@@ -9,11 +10,13 @@ class Croupier
 
   def self.highest_card(a_hand)
     cards = a_hand.split(' ')
-    face_values = ["2","3","4","5","6","7","8","9","T","J","Q","K","A"]
     ordered = cards.sort do |a_card, another_card|
-      comparison = face_values.index(a_card[0,1]) - face_values.index(another_card[0,1]) 
+      comparison = value(a_card) - value(another_card) 
     end
     ordered.last
   end
 
+  def self.value card
+    FACE_VALUES.index(card[0,1])
+  end 
 end
