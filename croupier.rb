026ae05c39d0408afcf9e147eead6d5c53743d37
XaV1Uzz <xaviuzz@gmail.com>
2014-07-08 13:22:16 +0200

    A croupier in the casino  when checking a player hand identifies the highest card REFAC

--- croupier.rb
+++ croupier.rb
@@ -1,6 +1,9 @@
 class Croupier
+  RANK="Highest card"
+  SEPARATOR=" : "
+
   def self.check_hand a_hand
-    return "Highest card : AS" if a_hand=="2H 3D AS 9C KD"
-    "Highest card : KD"
+    return RANK+ SEPARATOR + "AS" if a_hand == "2H 3D AS 9C KD"
+    RANK+ SEPARATOR + "KD"
   end
 end
