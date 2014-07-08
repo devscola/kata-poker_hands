a21c33e10ef51326fb9c17d736079a68919a281e
XaV1Uzz <xaviuzz@gmail.com>
2014-07-08 13:25:53 +0200

    A croupier in the casino  when checking a player hand identifies the highest card GREEN

--- croupier.rb
+++ croupier.rb
@@ -3,6 +3,7 @@ class Croupier
   SEPARATOR=" : "
 
   def self.check_hand a_hand
+    return RANK+ SEPARATOR + "9C" if a_hand == "2H 3D 4H 9C 6D"
     return RANK+ SEPARATOR + "AS" if a_hand == "2H 3D AS 9C KD"
     RANK+ SEPARATOR + "KD"
   end
