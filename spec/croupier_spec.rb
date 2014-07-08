c8536debb9d0771a07e884dad74885a826c4841c
XaV1Uzz <xaviuzz@gmail.com>
2014-07-08 17:03:14 +0200

    A croupier in the casino  when checking two players hands identifies the winner with highest card REFAC

--- spec/croupier_spec.rb
+++ spec/croupier_spec.rb
@@ -1,7 +1,7 @@
 require './croupier'
 
 describe "A croupier in the casino " do
-  
+
 	context 'when checking a player hand' do
     it "identifies the highest card" do
       expect(Croupier.check_hand("2H 3D 5S 9C KD")).to eq("Highest card : KD")
@@ -13,9 +13,11 @@ describe "A croupier in the casino " do
   context 'when checking two players hands' do
     it "identifies the winner with highest card " do
       expected = "Player one wins with Highest card : KD"
+      reverse_expected = "Player two wins with Highest card : KD"
       player_one = "2H 3D 5S 9C KD"
       player_two = "2H 3D 4H 9C 6D"
       expect(Croupier.check(player_one,player_two)).to eq(expected)
+      expect(Croupier.check(player_two,player_one)).to eq(expected)
     end
   end
 
