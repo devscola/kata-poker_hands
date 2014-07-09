517fd41bfe0adcbda9894a4d7e0eb542efcf3d87
XaV1Uzz <xaviuzz@gmail.com>
2014-07-09 19:30:51 +0200

    A poker Hand can be compared by best card values RED

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -12,5 +12,12 @@ describe "A poker Hand" do
     expect(a_hand <=> a_hand).to eq(0)
   end
 
+  it "can be compared by best card values" do
+    a_hand = Hand.new("3H 4D 5S 9C KD")
+    another_hand = Hand.new("2H 3D 5S 9C KD")
+    expect(a_hand <=> another_hand).to eq(1)
+    expect(another_hand <=> a_hand).to eq(-1)
+    expect(a_hand <=> a_hand).to eq(0)
+  end
 
 end
