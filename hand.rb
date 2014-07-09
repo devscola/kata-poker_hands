cc02f5ce6e5e8277b76a91e9706f134f4c11a756
XaV1Uzz <xaviuzz@gmail.com>
2014-07-09 19:17:43 +0200

    A poker Hand knows its highest card GREEN

--- hand.rb
+++ hand.rb
@@ -1,7 +1,11 @@
 class Hand
   def initialize hand_description
+    cards_descriptions = hand_description.split(' ')
+    @cards = cards_descriptions.map { |description| Card.new(description) }
+    @cards = @cards.sort
   end
+
   def highest_card
-    
+    @cards.last
   end
 end
